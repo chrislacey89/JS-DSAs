cff62ee44aefef98b19d427eb261626e16841836
Chris Lacey <clacey@fusionalliance.com>
2023-01-03 15:53:34 -0500

    update dfs

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function semordnilap(words) {
-  // Write your code here.
-  const resultMap = {};
-  const results = [];
-  for (let i = 0; i < words.length; i++) {
-    resultMap[words[i]] = 1;
-    const reversedString = words[i].split("").reverse("").join("");
-    if (resultMap[reversedString]) {
-      results.push([words[i], reversedString]);
-    }
-  }
-  return results;
-}
-
-const words = ["abc", "diaper", "cba", "repaid"];
-console.log(semordnilap(words));
+import { Node } from "./search/depthFirstSearch.js";
--- search/depthFirstSearch.js
+++ search/depthFirstSearch.js
@@ -1,4 +1,4 @@
-class Node {
+export class Node {
   constructor(name) {
     this.name = name;
     this.children = [];
@@ -18,3 +18,70 @@ class Node {
     return array;
   }
 }
+// Test for graph
+const graph = new Node("A");
+graph.addChild("B").addChild("C").addChild("D");
+graph.children[0].addChild("E").addChild("F");
+graph.children[2].addChild("G").addChild("H");
+graph.children[0].children[1].addChild("I").addChild("J");
+graph.children[2].children[0].addChild("K");
+console.log(JSON.stringify(graph));
+console.log(graph.depthFirstSearch([]));
+
+// ! Expected Output
+// [
+//   'A', 'B', 'E', 'F',
+//   'I', 'J', 'C', 'D',
+//   'G', 'K', 'H'
+// ]
+
+// Example graph that is generated from code above:
+const graphToSearch = {
+  name: "A",
+  children: [
+    {
+      name: "B",
+      children: [
+        {
+          name: "E",
+          children: [],
+        },
+        {
+          name: "F",
+          children: [
+            {
+              name: "I",
+              children: [],
+            },
+            {
+              name: "J",
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "C",
+      children: [],
+    },
+    {
+      name: "D",
+      children: [
+        {
+          name: "G",
+          children: [
+            {
+              name: "K",
+              children: [],
+            },
+          ],
+        },
+        {
+          name: "H",
+          children: [],
+        },
+      ],
+    },
+  ],
+};
