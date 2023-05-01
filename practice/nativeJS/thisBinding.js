c84d2bc633c9b26a05fff32d2238750661c2dc8d
Chris Lacey <clacey@fusionalliance.com>
2023-05-01 11:54:09 -0400

    add some logging

--- practice/nativeJS/thisBinding.js
+++ practice/nativeJS/thisBinding.js
@@ -26,3 +26,20 @@ Function.prototype.myBind = function (thisContext, ...args) {
   // Below is an alternative solution using `myCall`.
   // return (...newArgs) => this.myCall(thisContext, ...args, ...newArgs);
 };
+
+const obj = { num: 0 };
+
+function logNums(x, y) {
+  console.log(this.num, x, y);
+}
+
+logNums.myCall(obj, 1, 2);
+
+logNums.myApply(obj, [1, 2]);
+
+const boundFunction = logNums.myBind(obj, 1);
+boundFunction(2);
+
+// 0 1 2
+// 0 1 2
+// 0 1 2
